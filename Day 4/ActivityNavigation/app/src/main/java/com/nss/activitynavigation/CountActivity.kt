6e4c34f23d5ae66c38c3cab4d4234daabf4c8766
Nishidh <shekhawatnishidh5@gmail.com>
2020-06-05 21:47:48 +0530

    Day 4 app created

---
+++ Day 4/ActivityNavigation/app/src/main/java/com/nss/activitynavigation/CountActivity.kt
@@ -0,0 +1,53 @@
+package com.nss.activitynavigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class CountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.counting)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val myToast = Toast.makeText(applicationContext, "Incremented!", Toast.LENGTH_SHORT)
+            myToast.show()
+            count()
+        }
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            val myToast = Toast.makeText(applicationContext, "Counter Reset to 0!", Toast.LENGTH_LONG)
+            myToast.show()
+            reset()
+        }
+        findViewById<Button>(R.id.button3).setOnClickListener {
+            val myToast = Toast.makeText(applicationContext, "Decremented!", Toast.LENGTH_LONG)
+            myToast.show()
+            minus()
+        }
+
+    }
+
+    fun count(){
+        val tv = findViewById<TextView>(R.id.textView2)
+        val countString = tv.text.toString()
+        var count = countString.toInt()
+        count++
+        tv.text = count.toString()
+    }
+    fun minus(){
+        val tv = findViewById<TextView>(R.id.textView2)
+        val countString = tv.text.toString()
+        var count = countString.toInt()
+        count--
+        tv.text = count.toString()
+    }
+    fun reset(){
+        val tv = findViewById<TextView>(R.id.textView2)
+        var count = 0
+        tv.text = count.toString()
+    }
+
+}
